f3e12ed1914e8687dfbd3c3bfa7bc090da7b50ee
benomase <benomarseifeddine@gmail.com>
2017-09-11 15:35:56 +0200

    ejb was succesfuly injected to web servlet

---
+++ WebContent/index.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Web Calculator</title>
+</head>
+<body>
+<h1>Web Calculator</h1>
+<form action="doadd" method="get">
+A: <br>
+<input type=text name="a"><Br><br>
+B: <br>
+<input type=text name="b"><Br>
+<br>
+<input type="submit" name=bt value="Submit">
+</form>
+</body>
+</html>
---
+++ src/com/logic/Doadd.java
@@ -0,0 +1,58 @@
+package com.logic;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Doadd
+ */
+@WebServlet("/Doadd")
+public class Doadd extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB(lookup ="ejb:/HotelBookersEJB//Rooms!com.logic.RoomsRemote")
+    private RoomsRemote abc;
+    
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Doadd() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String as=request.getParameter("a");
+		int a=Integer.parseInt(as);
+		as=request.getParameter("b");
+		int b=Integer.parseInt(as);
+		
+		int result=0;
+		
+		try {
+			result=abc.add(a, b);
+		}catch(Exception e){e.printStackTrace();}
+ 
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("<h3>Results for the addition</h3>");
+		out.println("Result:"+result);		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
