8645267a0aa4acd7f6c4dfa25939dbb624c750cf
karim <abdelkarim.drareni@hotmail.com>
2017-09-26 18:12:42 +0200

    Administration add room

--- src/com/controllers/AdministrationController.java
+++ src/com/controllers/AdministrationController.java
@@ -29,7 +29,7 @@ public class AdministrationController {
 	
 	@POST
 	@Path("/etatChange")
-	@Produces("application/json")//dont use refresh token now
+	@Produces("application/json")//dont use refresh token now havn't time to do it
 	public JsonResult modifyEtatChambre(@FormParam("token") String token,@DefaultValue("-1") @FormParam("idChambre") Long idChambre) {
 		//verifier champs non vide
 		if(token==null||token.isEmpty()|| idChambre==-1)
@@ -45,5 +45,29 @@ public class AdministrationController {
 		//modifer
 		administrationRemote.modifyEtatChambre(idChambre);
 		return new JsonResult(201, "Modification de l'etat de la chambre reussi");
+	}
+	@POST
+	@Path("/addRoom")
+	@Produces("application/json")
+	public JsonResult addRoom(@FormParam("token") String token,
+							  @DefaultValue("-1") @FormParam("numero") Long num,
+							  @DefaultValue("-1") @FormParam("nbLits") int nbLits,
+							  @DefaultValue("-1") @FormParam("etage") Long etage ,
+							  @DefaultValue("-1") @FormParam("etat") int etat,
+							  @DefaultValue("-1") @FormParam("prix") float prix,
+							  @DefaultValue("-1") @FormParam("idHotel") Long idHotel) {
+		//test if champ not null et not empty
+		
+		//valid token and user admin
+		String idUser=secur.validateToken(token);
+		
+		if(idUser==null || idUser.isEmpty())
+			return new JsonResult(401, "user not connected!");
+		Utilisateur u=authenticationRemote.getUserFromId(Long.parseLong(idUser));
+		if(!u.getRole().getlibelle().equals("ROLE_ADMIN"))
+			return new JsonResult(401, "Vous n'avez pas le droit d'executer cette action");
+		//add room
+		administrationRemote.addRoom( num, nbLits, etage, etat, prix,  idHotel);
+		return new JsonResult(201, "chambre ajoutee avec succes");
 	}
 }
